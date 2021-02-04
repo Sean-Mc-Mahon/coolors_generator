c4499c0acde5acad97f2368f7448a6ae5d1cca74
Sean McMahon <sean.mcmahon87@gmail.com>
2021-02-04 19:53:43 +0000

    functions to gen. Hex Values & assign them to Divs

---
+++ app.js
@@ -0,0 +1,33 @@
+//Global selections and variables
+const colorDivs = document.querySelectorAll('.color');
+const generateBtn = document.querySelector('.generate');
+const sliders = document.querySelectorAll('input[type="range"]');
+const currentHexes = document.querySelectorAll('.color h2');
+let initialColors;
+
+
+//Functions
+
+//Color Generator
+function generateHex() {
+  const letters = '0123456789ABCDEF';
+  let hash = '#';
+  for(let i = 0; i < 6; i++){
+    hash += letters[Math.floor(Math.random() * 16)];
+  }
+  return hash;
+}
+let randomHex = generateHex();
+console.log(randomHex);
+
+function randomColors() {
+  colorDivs.forEach((div, index) => {
+    const hexText = div.children[0];
+    const randomColor = generateHex();
+
+    //Add the color to the bg
+    div.style.backgroundColor = randomColor;
+    hexText.innerText = randomColor;
+  });
+}
+randomColors();
